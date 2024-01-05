5f8bd0f71178d7c63e702ad181d4b08b2a627bf0
RevealSquid <hubertle43100@gmail.com>
2024-01-04 21:48:29 -0800

    Fixing push placment

---
+++ minimal-blog/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ebpdy293',
+    dataset: 'production'
+  }
+})
---
+++ minimal-blog/schemas/post.ts
@@ -0,0 +1,55 @@
+export default {
+  name: 'post',
+  type: 'document',
+  title: 'Post',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'overview',
+      type: 'string',
+      title: 'Overview',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title',
+      },
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        {
+          type: 'block',
+        },
+        {
+          type: 'image',
+          fields: [
+            {
+              type: 'text',
+              name: 'alt',
+              title: 'Alternative Text',
+            },
+          ],
+        },
+        {
+          type: 'code',
+          block: [
+            {
+              name: 'code',
+              title: 'code block',
+              type: 'code',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
