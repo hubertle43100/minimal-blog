ffa549ba227c3ba0c3f86d6178380c83626107aa
RevealSquid <hubertle43100@gmail.com>
2024-01-31 13:32:02 -0800

    🔨got rid of route.ts

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,21 +1,22 @@
-import { Resend } from "resend";
-import AvocadoEmail from "@/app/emails/Avocado";
-import { NextResponse } from "next/server";
+// import { Resend } from "resend";
+// import AvocadoEmail from "@/app/emails/Avocado";
+// import { NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function GET() {
-  try {
-    const data = await resend.emails.send({
-      from: "email@mail.avocado.dev",
-      to: "hubertle43100@gmail.com",
-      subject: "Hello World",
-      react: AvocadoEmail(),
-    });
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({
-      status: "UMM...",
-    });
-  }
-}
+// export async function GET() {
+//   try {
+//     const data = await resend.emails.send({
+//       from: "email@mail.avocado.dev",
+//       to: "hubertle43100@gmail.com",
+//       subject: "Hello World",
+//       react: AvocadoEmail(),
+//     });
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({
+//       status: "UMM...",
+//     });
+//   }
+// }
+export {};
